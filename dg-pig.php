fd10195507496554c0c8f88e727d4fd48275a0bc
Rover Chen <rover.k.chen@gmail.com>
2021-08-18 07:39:04 +0800

    init

---
+++ dg-pig.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Plugin Name: dg-pig
+ * Plugin URI: https://wordpress.org/plugins/dg-pig/
+ * Description: The leading web api plugin for pig system by shortcode
+ * Author: dgc.network
+ * Author URI: https://dgc.network/
+ * Version: 1.0.0
+ * Requires at least: 4.4
+ * Tested up to: 5.2
+ * 
+ * Text Domain: dg-pig
+ * Domain Path: /languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/*
+ * Define DG_PIG_PLUGIN_FILE.
+ */
+if ( ! defined( 'DG_PIG_PLUGIN_FILE' ) ) {
+    define( 'DG_PIG_PLUGIN_FILE', __FILE__ );
+}
+
+/*
+ * Include the main class.
+ */
+if ( ! class_exists( 'dg_pig' ) ) {
+    include_once dirname( __FILE__ ) . '/includes/class-dg-pig.php';
+}
+
+/*
+function dg_pig(){
+    return dg_pig::instance();
+}
+
+$GLOBALS['dg-pig'] = dg_pig();
+*/
+
+?>
