ae4300ad0c4bb13ee75663cf98aedfd4f69f24eb
Rover Chen <rover.k.chen@gmail.com>
2021-08-31 16:38:24 +0800

    include_once dirname( __FILE__ ) . '/php-OP_RETURN/OP_RETURN.php';

--- smart-farm.php
+++ smart-farm.php
@@ -19,6 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 include_once dirname( __FILE__ ) . '/php-OP_RETURN/OP_RETURN.php';
+include_once dirname( __FILE__ ) . '/build/gen/Agent.php';
 $from = new Agent();
 
 add_shortcode( 'shortcode_name', 'shortcode_handler_function' );
