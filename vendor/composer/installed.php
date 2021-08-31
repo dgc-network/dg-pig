34df13f186767ecdd51c3fc701690dc5b8cf3523
Rover Chen <rover.k.chen@gmail.com>
2021-08-31 16:55:56 +0800

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.41.0',
+        'version' => '1.41.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'grpc/grpc-dev',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'google/protobuf' => array(
+            'pretty_version' => 'v3.17.3',
+            'version' => '3.17.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../google/protobuf',
+            'aliases' => array(),
+            'reference' => 'ae9282cf11dd2933b7e71a611f9590f07d53d3f3',
+            'dev_requirement' => false,
+        ),
+        'grpc/grpc-dev' => array(
+            'pretty_version' => '1.41.0',
+            'version' => '1.41.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
